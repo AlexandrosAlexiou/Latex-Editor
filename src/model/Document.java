d3be30765e2393b373383d127cf6f57071157625
AlexandrosAlexiou <alexandros.alexiou@yahoo.com>
2019-04-07 19:11:59 +0300

    Basic structure of Document class

--- src/model/Document.java
+++ src/model/Document.java
@@ -1,6 +1,32 @@
 package model;
 
+
 public class Document {
 
+    private String contents;
+    private String versionID;
 
+    private Document(String contents, String versionID){
+        this.contents=contents;
+        this.versionID=versionID;
+    }
+
+    public void setContents(String template){
+
+    }
+
+    public String getContents(){
+        String lala="lala";
+        return lala;
+    }
+
+    public void save(){
+
+    }
+
+    public Document cloneDeep(){
+
+        return new Document(new String(this.contents),new String(this.versionID));
+
+    }
 }

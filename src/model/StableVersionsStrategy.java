e4e36783f070f7391e4def00eb78ab6423ff11f4
AlexandrosAlexiou <alexandros.alexiou@yahoo.com>
2019-05-15 20:01:49 +0300

    Implementing Stable Versioning that saves the Document versions on disk

--- src/model/StableVersionsStrategy.java
+++ src/model/StableVersionsStrategy.java
@@ -1,6 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class StableVersionsStrategy implements VersionsStrategy {
+
+    private ArrayList<Document> versionsHistory = new ArrayList<Document>();
 
     public StableVersionsStrategy(){}
 
